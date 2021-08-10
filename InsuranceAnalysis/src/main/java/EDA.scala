c620b9e529617dabcea1215ce47d72843aeeaaa7
IAmMsx <894572503@qq.com>
2021-08-10 17:34:39 +0800

    8.10 think

--- InsuranceAnalysis/src/main/java/EDA.scala
+++ InsuranceAnalysis/src/main/java/EDA.scala
@@ -9,6 +9,23 @@ object EDA {
         val df: DataFrame = Preprocessing.trainInput
         df.show()
 
+        // df.printSchema() 显示dafaFrame的结构
+//        println(df.printSchema())
+//        df.printSchema()
+//
+//        println("*******************")
+//        println(df.count())
+//
+//        df.select("id","cat1","cat2","cat3","cont2","cont3","loss")
+//          .show()
+
+        val newDF = df.withColumnRenamed("loss", "label")
+        newDF.createOrReplaceTempView("insurance")
+
+        spark.sql("SElECT avg(insurance.label) as AVG_LOSS FROM insurance").show()
+        spark.sql("SElECT min(insurance.label) as MIN_LOSS FROM insurance").show()
+        spark.sql("SElECT max(insurance.label) as MAX_LOSS FROM insurance").show()
+
 
     }
 
--- InsuranceAnalysis/src/main/java/Preprocessing.scala
+++ InsuranceAnalysis/src/main/java/Preprocessing.scala
@@ -1,6 +1,8 @@
-import org.apache.spark.sql.{DataFrame, SparkSession}
+import org.apache.spark.sql.{DataFrame, Dataset, Row, SparkSession}
 
 object Preprocessing {
+    val trainSample = 1.0
+    val testSample = 1.0
     val train = "data/train.csv"
     val test = "data/test.csv"
 
@@ -14,5 +16,33 @@ object Preprocessing {
       .format("csv")
       .load(train)
       .cache()
+
+    val testInput: DataFrame = spark.read
+      .option("header", "true")
+      .option("inferSchema", "true") //避免将浮点值视为字符串
+      .format("csv")
+      .load(test)
+      .cache()
+
+    println("Preparing data for training model")
+    var data: Dataset[Row] = trainInput.withColumnRenamed("loss", "label")
+      .sample(withReplacement = false, trainSample)
+
+    // null check
+    val DF: DataFrame = data.na.drop()
+    if (DF == data)
+        println("No null values in the DataFrame")
+    else{
+        println("Null values exist in the DataFrame")
+        data = DF
+    }
+    val seed = 12345L
+    val splits: Array[Dataset[Row]] = data.randomSplit(Array(0.75,0.25),seed)
+    val (trainingData,validationData) = (splits(0),splits(1))
+
+    trainingData.cache()
+    validationData.cache()
+
+
 
 }
